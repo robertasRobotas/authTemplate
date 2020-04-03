18371941668c7f975e3cd4f21d3d7b9fcf01dddc
Robertas <robertas.ankudovicius@ibm.com>
2020-04-03 22:47:08 +0100

    added some files reated with roting

--- front/src/App.js
+++ front/src/App.js
@@ -1,52 +1,13 @@
 import React from 'react';
 import axios from 'axios';
+import Router from './router';
 
-function App() {
-  const apiCall = () => {
-    axios
-      .get('http://localhost:8085/user/redirect', { withCredentials: true })
-      .then((res) => {
-        console.log(res);
-      });
-  };
+const App = () => {
   return (
-    <div className='App'>
-      <a
-        href='http://localhost:8085/google/auth'
-        style={{
-          display: 'block',
-          backgroundColor: 'red',
-          width: '100px',
-          height: '100px'
-        }}></a>
-      <a
-        href='http://localhost:8085/google/logout'
-        style={{
-          display: 'block',
-          backgroundColor: 'grey',
-          width: '100px',
-          height: '100px'
-        }}></a>
-      <a
-        onClick={apiCall}
-        style={{
-          display: 'block',
-          backgroundColor: 'black',
-          width: '100px',
-          height: '100px'
-        }}></a>
-      <a
-        href='http://localhost:8085/facebook/auth'
-        style={{
-          display: 'block',
-          backgroundColor: 'blue',
-          width: '100px',
-          height: '100px'
-        }}>
-        facebook
-      </a>
-    </div>
+    <>
+      <Router />
+    </>
   );
-}
+};
 
 export default App;
---
+++ front/src/pages/Login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import axios from 'axios';
+
+function Login() {
+  const apiCall = () => {
+    axios
+      .get('http://localhost:8085/user/redirect', { withCredentials: true })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+  return (
+    <div className='App'>
+      <a
+        href='http://localhost:8085/google/auth'
+        style={{
+          display: 'block',
+          backgroundColor: 'red',
+          width: '100px',
+          height: '100px',
+        }}></a>
+      <a
+        href='http://localhost:8085/google/logout'
+        style={{
+          display: 'block',
+          backgroundColor: 'grey',
+          width: '100px',
+          height: '100px',
+        }}></a>
+      <a
+        onClick={apiCall}
+        style={{
+          display: 'block',
+          backgroundColor: 'black',
+          width: '100px',
+          height: '100px',
+        }}></a>
+      <a
+        href='http://localhost:8085/facebook/auth'
+        style={{
+          display: 'block',
+          backgroundColor: 'blue',
+          width: '100px',
+          height: '100px',
+        }}>
+        facebook
+      </a>
+      <div id='my-signin2'></div>
+    </div>
+  );
+}
+
+export default Login;
