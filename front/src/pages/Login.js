4c672b9243ac914d3668e581ddfa28ae97c8c058
Robertas <robertas.ankudovicius@ibm.com>
2020-04-04 23:02:37 +0100

    styled components added / button formating

--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -4,19 +4,37 @@ import {
   FacebookLoginButton,
   GoogleLoginButton,
 } from 'react-social-login-buttons';
+import styled from 'styled-components';
 
 function Login() {
-  const style = { textDecoration: 'none' };
+  const style = { textDecoration: 'none', textAlign: 'center', width: '10px' };
+
+  const ButtonWrapper = styled.div`
+    margin-top: 10em;
+    text-align: center;
+  `;
+
+  const Form = styled.div`
+    position: relative;
+    width: 50%;
+    left: 50%;
+    transform: translateX(-50%);
+  `;
+
   return (
-    <>
+    <Form>
       <a style={style} href='http://localhost:8085/google/auth'>
-        <GoogleLoginButton />
+        <ButtonWrapper>
+          <GoogleLoginButton />
+        </ButtonWrapper>
       </a>
 
       <a style={style} href='http://localhost:8085/facebook/auth'>
-        <FacebookLoginButton />
+        <ButtonWrapper>
+          <FacebookLoginButton />
+        </ButtonWrapper>
       </a>
-    </>
+    </Form>
   );
 }
 
