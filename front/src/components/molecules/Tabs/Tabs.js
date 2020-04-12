a3790eacfb2745a7fea79d1ad7a9b43dc8419cac
Robertas <r.ankudovicius@gmail.com>
2020-04-12 23:11:48 +0300

    Deleted distractive styles

--- front/src/components/molecules/Tabs/Tabs.js
+++ front/src/components/molecules/Tabs/Tabs.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from '../../atoms';
-import styles from './Tabs.module.css';
-import './Tabs.module.css';
-
-const ResponsiveLinks = styled.div`
-  margin: 2em;
-  display: grid;
-
-  @media only screen and (min-width: 600px) {
-    grid-template-columns: repeat(3, 1fr);
-    margin: 0;
-  }
-`;
+import './Tabs.css';
 
 const NavBarTabs = ({ navLinks, menuStatus }) => {
   return (
-    <ul className={menuStatus ? styles.open : styles.closed}>
-      <ResponsiveLinks>
+    <ul className={menuStatus ? 'open' : 'closed'}>
+      <div className='ResponsiveLinks'>
         {navLinks.map((link, i) => (
           <Link key={i} path={link.path} text={link.text} />
         ))}
-      </ResponsiveLinks>
+      </div>
     </ul>
   );
 };
--- front/src/components/organisms/navbar/navbar.js
+++ front/src/components/organisms/navbar/navbar.js
@@ -4,35 +4,23 @@ import navLinks from './navLinks';
 import { Tabs, UserDropDown } from '../../molecules';
 import { Burger, Logo } from '../../atoms';
 import { getUserData } from '../../../helpers/functions';
-
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  background-color: brown;
-  height: 4em;
-`;
+import './Navbar.css';
 
 const Navbar = () => {
   const [menuStatus, setMenuStatus] = useState(false);
-  const userData = getUserData();
-  console.log('HOOKuserData', userData);
 
-  // useEffect(() => {
-  //   setUserData(getUserData());
-  // }, [JSON.parse(localStorage.getItem('userData'))]);
   const changeMenuStatus = () => {
     setMenuStatus(!menuStatus);
-    console.log(menuStatus);
   };
 
   return (
     <>
-      <Wrapper>
+      <div className='NavbarContainer'>
         <Logo />
         <Burger changeMenuStatus={changeMenuStatus} />
         <Tabs navLinks={navLinks} menuStatus={menuStatus} />
         <UserDropDown />
-      </Wrapper>
+      </div>
     </>
   );
 };
